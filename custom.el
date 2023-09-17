8d092803097439849bcd342a2ee67a8365e93781
Mark Olson <41911657+mholson@users.noreply.github.com>
2023-09-17 14:27:36 +0200

    adding the custom *.el files

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "554ca5fb1eecd56f1af30610864ff50bf4b9058060af79cf4585c10b06092c0b" "7ea883b13485f175d3075c72fceab701b5bf76b2076f024da50dff4107d0db25" "8b130d9b896ba883fc99ebfbf1bba5600e228793bd08eebd89e239daecd9d7af" "f5a7c10fbb726e90a3befca828b2a4de493404a13ce42dcbeb5212ecd553189e" "6431197c878808fafc11ee1278bf21b709537d1fb377bc32bfcf7a5cfbeb3d63" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "34898c07de71bfafdfa1f83fd9b88ce85ec8428607d52e94ce37780cc7d85263" "aa3fb68b03373c3dc9c33d8aea5f0ee3b99ffa9e88c8c7fc07bee08ec9447429" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
+ '(warning-suppress-types '((server) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-modified ((t (:foreground "orange")))))
